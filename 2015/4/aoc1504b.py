2bb120e673423f3d7784ca8243acc6affaff4ecb
Jonas Thorsell <jonas.thorsell@gmail.com>
2021-11-07 14:32:42 +0100

    AoC 2015 day 4

---
+++ 2015/4/aoc1504b.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2021 Jonas Thorsell
+import sys
+import itertools
+import hashlib
+
+for key in sys.stdin:
+    key = key.strip()
+    for n in itertools.count(start=1):
+        s = key + str(n)
+        md5 = hashlib.md5(s).hexdigest()
+        if md5[:6] == "000000":
+            print(n)
+            print("(s: " + key + str(n) + "\tmd5: " + md5 + ")")
+            break
