447841ba9f3a0f3497f1d6464bad241434d5ce4b
Jonas Thorsell <jonas.thorsell@gmail.com>
2021-12-06 10:07:59 +0100

    AoC 2021 Day 6

---
+++ 2021/6/aoc2106a.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 Jonas Thorsell
+import sys
+from collections import defaultdict
+
+tl = [0] * 9
+for t in [int(x) for x in sys.stdin.readline().split(',')]:
+    tl[t] += 1
+
+for d in range(80):
+    ntl = [0] * 9
+    for i in range(9):
+        if i == 0:
+            ntl[8] = tl[0]
+            ntl[6] = tl[0]
+        else:
+            ntl[i-1] += tl[i]
+    tl = ntl
+
+print(sum(tl))
