5e790750153f03a378e1c02b03e69210c526b729
Jonas Thorsell <jonas.thorsell@gmail.com>
2021-12-08 17:24:10 +0100

    AoC 2021 Day 8 part 2

--- 2021/8/aoc2108b.py
+++ 2021/8/aoc2108b.py
@@ -1,20 +1,13 @@
 # Copyright (c) 2021 Jonas Thorsell
 import sys
 
-#  AAA
-# F   B
-# F   B
-#  GGG
-# E   C
-# E   C
-#  DDD
-c = 0
+total = 0
 for l in sys.stdin:
     s =[set('abcdefg')] * 7
     t = l.strip().split('|')
     d = t[0].split()
     o = t[1].split()
-    for x in (d + o):
+    for x in d:
         if len(x) == 2: # 1
             s[0] = s[0] - set(x)
             s[1] = s[1] & set(x)
@@ -43,17 +36,16 @@ for l in sys.stdin:
             s[0] = s[0] & set(x)
             s[3] = s[3] & set(x)
             s[6] = s[6] & set(x)
-            if not s[1] & set(x): # 5
-                s[4] = s[4] - set(x)
-            if not s[2] & set(x): # 2
-                s[5] = s[5] - set(x)
         elif len(x) == 6: # 6,9,0
             s[0] = s[0] & set(x)
             s[2] = s[2] & set(x)
             s[3] = s[3] & set(x)
             s[5] = s[5] & set(x)
-        #else 8
-    print(s)
+    for i in range(7):
+        if len(s[i]) == 1:
+            for j in range(7):
+                if j != i: s[j] = s[j] - s[i]
+    value=0
     for x in o:
         p = [len(w & set(x))>0 for w in s]
         n = -1
@@ -61,7 +53,6 @@ for l in sys.stdin:
         elif len(x) == 3: n = 7
         elif len(x) == 4: n = 4
         elif len(x) == 5: # 2,3,5
-            n = 235
             if not p[1]: n = 5
             elif not p[2]: n = 2
             else: n = 3
@@ -70,4 +61,6 @@ for l in sys.stdin:
             elif not p[4]: n = 9
             elif not p[6]: n = 0
         else: n = 8
-        print(x, n)
+        value=value*10+n
+    total += value
+print(total)
