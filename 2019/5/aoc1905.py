99b1476d54e94518d5ec009790fcebe407900858
Jonas Thorsell <jonas.thorsell@gmail.com>
2024-12-03 07:40:37 +0100

    AoC 2019 Day 5 part 1

---
+++ 2019/5/aoc1905.py
@@ -0,0 +1,58 @@
+import sys
+
+def run(m):
+    ip = 0
+    while (not m[ip] == 99):
+        op = m[ip] % 100
+        m1 = (m[ip] // 100) % 10
+        m2 = (m[ip] // 1000) % 10
+        m3 = (m[ip] // 10000) % 10
+
+        # Add
+        if op == 1:
+            p1,p2,p3 = m[ip+1:ip+4]
+            if not m1:
+                p1 = m[p1]
+            if not m2:
+                p2 = m[p2]
+            if m3:
+                raise NotImplementedError('Intcode: Invalid Mode ' + str(m[ip]) + ' @ ' + str(ip))
+            m[p3] = p1 + p2
+            ip += 4
+        
+        # Multipli
+        elif op == 2:
+            p1,p2,p3 = m[ip+1:ip+4]
+            if not m1:
+                p1 = m[p1]
+            if not m2:
+                p2 = m[p2]
+            if m3:
+                raise NotImplementedError('Intcode: Invalid Mode ' + str(m[ip]) + ' @ ' + str(ip))
+            m[p3] = p1 * p2
+            ip += 4
+        
+        # Input
+        elif op == 3:
+            p1 = m[ip+1]
+            if m1:
+                raise NotImplementedError('Intcode: Invalid Mode ' + str(m[ip]) + ' @ ' + str(ip))
+            m[p1] = int(input())
+            ip += 2
+        
+        # Output
+        elif op == 4:
+            p1 = m[ip+1]
+            if not m1:
+                p1 = m[p1]
+            print(p1)
+            ip += 2
+        
+        else:
+            raise NotImplementedError('Intcode: Invalid Opcode ' + str(m[ip]) + ' @ ' + str(ip))
+
+with open(sys.argv[1]) as f:
+    orgmem = [int(x) for x in f.read().split(',')]
+
+m = orgmem.copy()
+run(m)
